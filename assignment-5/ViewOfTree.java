50f1aea8e5cdf6286a6668d548a8cce03881a1ae
Rishi <61271406+rishi7pillai@users.noreply.github.com>
2021-02-21 21:37:11 +0530

    Create ViewOfTree.java

---
+++ assignment-5/ViewOfTree.java
@@ -0,0 +1,89 @@
+import java.util.*; 
+import java.util.Map.Entry; 
+  
+
+   
+class Tree 
+{ 
+    TreeNode root; //root node of tree 
+  
+    // Default constructor 
+    public Tree() {} 
+  
+    // Parameterized tree constructor 
+    public Tree(TreeNode node) 
+    { 
+        root = node; 
+    } 
+  
+     
+    public static void bottomView() 
+    { 
+        if (root == null) 
+            return; 
+  
+        
+        int hd = 0; 
+  
+       
+        Map<Integer, Integer> map = new TreeMap<>(); 
+  
+         
+        Queue<Node> queue = new LinkedList<Node>(); 
+  
+        
+        root.hd = hd; 
+        queue.add(root); 
+  
+      
+        while (!queue.isEmpty()) 
+        { 
+            Node temp = queue.remove(); 
+  
+            
+            hd = temp.hd; 
+            map.put(hd, temp.data); 
+  
+            
+            if (temp.left != null) 
+            { 
+                temp.left.hd = hd-1; 
+                queue.add(temp.left); 
+            } 
+     
+            if (temp.right != null) 
+            { 
+                temp.right.hd = hd+1; 
+                queue.add(temp.right); 
+            } 
+        }  
+        Set<Entry<Integer, Integer>> set = map.entrySet(); 
+  
+        Iterator<Entry<Integer, Integer>> iterator = set.iterator(); 
+  
+        while (iterator.hasNext()) 
+        { 
+            Map.Entry<Integer, Integer> me = iterator.next(); 
+            System.out.print(me.getValue()+" "); 
+        } 
+    } 
+} 
+  
+public class BottomView 
+{ 
+    public static void main(String[] args) 
+    { 
+        Node root = new Node(20); 
+        root.left = new Node(8); 
+        root.right = new Node(22); 
+        root.left.left = new Node(5); 
+        root.left.right = new Node(3); 
+        root.right.left = new Node(4); 
+        root.right.right = new Node(25); 
+        root.left.right.left = new Node(10); 
+        root.left.right.right = new Node(14); 
+    
+        System.out.println("Bottom view of the given binary tree:"); 
+        bottomView(); 
+    } 
+}
