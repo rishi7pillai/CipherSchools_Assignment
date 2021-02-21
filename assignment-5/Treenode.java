37f569456afba2a8d00dd719470628a901c03c87
Rishi <61271406+rishi7pillai@users.noreply.github.com>
2021-02-21 19:48:09 +0530

    Create Treenode.java

---
+++ assignment-5/Treenode.java
@@ -0,0 +1,16 @@
+package Trees;
+
+public class TreeNode {
+
+    public int data;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    TreeNode next;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
