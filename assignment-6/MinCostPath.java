8a8e8c0cc2a4bdc61b8ab39915a981abcf6ae623
Rishi <61271406+rishi7pillai@users.noreply.github.com>
2021-02-23 23:30:56 +0530

    Create MinCostPath.java

---
+++ assignment-6/MinCostPath.java
@@ -0,0 +1,39 @@
+package DP;
+
+public class MinCostPath {
+
+    public static int minPathSum(int[][] grid)
+    {
+        int m = grid.length; // row
+        int n = grid[0].length; // column
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j != 0) {
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                }
+                else if (i != 0 && j == 0) {
+                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                }
+                else if (i == 0 && j == 0) {
+                    grid[i][j] = grid[i][j];
+                }
+                else {
+                    grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j])
+                            + grid[i][j];
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] cost = { {1, 3, 1},
+                        {1, 5, 1},
+                        {4, 2, 1} };
+
+        System.out.println(minPathSum(cost));
+    }
+}
